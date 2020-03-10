baf0d21a71c0b6c0cdd0ea9ebbf23b061d21c8bb
SurinMax <60728730+SurinMax@users.noreply.github.com>
2020-03-10 09:36:14 +0300

    ООП для генерации примеров

---
+++ jMathGenerator/src/jMathGenerator.java
@@ -0,0 +1,131 @@
+import javax.xml.crypto.Data;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
+
+public class jMathGenerator {
+    private int firstNumber;
+    private int secondNumber;
+    private String matchOperation;
+    private int startRange;
+    private int endRange;
+
+    /**
+     * Конструктор
+     * @param uStartRange начала диапазона
+     * @param uEndRange конец диапазона
+     * @param uMatchOperation знак математической операции
+     */
+    jMathGenerator(int uStartRange, int uEndRange, String uMatchOperation){
+        matchOperation = uMatchOperation;
+        startRange = uStartRange;
+        endRange = uEndRange;
+    }
+
+    /**
+     * Генерирует первое число для примера
+     */
+    public void genFirstNumber(){
+        Random random = new Random();
+        firstNumber = random.nextInt((endRange - startRange) + 1) + startRange;
+    }
+
+    /**
+     * Генерирует второе число для примера
+     */
+    public void genSecondNumber(){
+        Random random = new Random();
+        secondNumber = random.nextInt((endRange - startRange) + 1) + startRange;
+    }
+
+    /**
+     * Метод проверяет правильность введенного диапазона генерации
+     * @return true/false
+     */
+    public boolean checkRange(){
+        return startRange < endRange;
+    }
+
+    /**
+     * Метод проверят корректсность сгенерированного примера. Применять только для примеров с вычетанием.
+     * @return true/false
+     */
+    public boolean checkGen(){
+        return firstNumber >= secondNumber;
+    }
+
+    /**
+     * Функцтя возвращает первое число в примере
+     * @return число
+     */
+    public int getFirstNumber() {
+        return firstNumber;
+    }
+
+    /**
+     * Функиия возвращает второе число в примере
+     * @return число
+     */
+    public int getSecondNumber() {
+        return secondNumber;
+    }
+
+    /**
+     * Функция возвращает знак математической операции
+     * @return знак операции
+     */
+    public String getMatchOperation() {
+        return matchOperation;
+    }
+
+    /**
+     * Функиия записи/дозаписи примеров в файл
+     * @param pathToFile путь к файлу
+     * @param lineWrite что пишем в файл
+     */
+    public void WriteToFile(String pathToFile, String lineWrite){
+        try {
+            FileWriter fw = new FileWriter(pathToFile,true);
+            fw.write(lineWrite);
+            fw.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String pathToFile = "E:\\test.txt";
+        int countMatch = 10;
+        System.out.println("Match Generator Start");
+        jMathGenerator mathGeneratorAdd = new jMathGenerator(10, 50,"+");
+        jMathGenerator mathGeneratorSub = new jMathGenerator(10,50,"-");
+        if(!mathGeneratorAdd.checkRange()){
+            System.out.println("Неверные начальные условия");
+            System.exit(0);
+        }
+        Date date = new Date();
+        mathGeneratorAdd.WriteToFile(pathToFile,date.toString()+"\n");
+        for (int i=1;i<=countMatch;i++){
+            mathGeneratorAdd.genFirstNumber();
+            mathGeneratorAdd.genSecondNumber();
+            do{
+                mathGeneratorSub.genFirstNumber();
+                mathGeneratorSub.genSecondNumber();
+            }
+            while (!mathGeneratorSub.checkGen());
+            mathGeneratorAdd.WriteToFile(pathToFile, i +": "+
+                                                            mathGeneratorAdd.getFirstNumber()+
+                                                            mathGeneratorAdd.getMatchOperation()+
+                                                            mathGeneratorAdd.getSecondNumber()+"=\n");
+            mathGeneratorSub.WriteToFile(pathToFile, i+": "+
+                    mathGeneratorSub.getFirstNumber()+
+                    mathGeneratorSub.getMatchOperation()+
+                    mathGeneratorSub.getSecondNumber()+"=\n");
+        }
+    }
+
+}
+
