61092c2189165f8189276046f72b0801de784964
SurinMax <60728730+SurinMax@users.noreply.github.com>
2020-03-22 10:35:20 +0300

    Next step
    Next step coding

--- jGUIMathGenerator/src/jGUIMathGenerator.java
+++ jGUIMathGenerator/src/jGUIMathGenerator.java
@@ -1,12 +1,46 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class jGUIMathGenerator extends JFrame {
 
+    public void windowsBuilder(){
+        JPanel jPanel = new JPanel(new GridBagLayout());
+        //JFrame jFrame = new JFrame();
+        //jFrame.add(jPanel);
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        jPanel.setLayout(gridBagLayout);
+
+        GridBagConstraints constraints1 = new GridBagConstraints();
+        constraints1.weightx=0;
+        constraints1.weighty=0;
+        constraints1.gridx=0;
+        constraints1.gridy=0;
+        constraints1.gridheight=1;
+        constraints1.gridheight=1;
+        JButton btGen = new JButton("Generate");
+
+        GridBagConstraints constraints2 = new GridBagConstraints();
+        constraints2.weightx=0;
+        constraints2.weighty=0;
+        constraints2.gridx=1;
+        constraints2.gridy=0;
+        constraints2.gridheight=1;
+        constraints2.gridheight=1;
+        JButton btExit = new JButton("Exit");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jPanel.add(btGen,constraints1);
+        jPanel.add(btExit,constraints2);
+
+        getContentPane().add(jPanel);
+        setSize(300,300);
+        setTitle("Math Generator");
+        setVisible(true);
+    }
+
     public static void main(String[] args) {
         jGUIMathGenerator frame = new jGUIMathGenerator();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,300);
-        frame.setTitle("My First Window");
-        frame.setVisible(true);
+        frame.windowsBuilder();
     }
 }
