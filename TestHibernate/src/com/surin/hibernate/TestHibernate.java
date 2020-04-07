13b5cc4fc480da0743b2bfea1abd72551e582abd
SurinMax <60728730+SurinMax@users.noreply.github.com>
2020-04-07 14:58:56 +0300

    Add distrib

--- TestHibernate/src/com/surin/hibernate/TestHibernate.java
+++ TestHibernate/src/com/surin/hibernate/TestHibernate.java
@@ -1,9 +1,53 @@
 package com.surin.hibernate;
 
-import org.hibernate.cfg.Configuration;
+import javax.persistence.Column;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.annotations.Columns;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class TestHibernate {
-	public static void main(String[] args) {
+	private int id;
+	private String city;
+	private String fio;
+	 
+	public TestHibernate(String city, String fio) {
+		this.city = city;
+		this.fio = fio;
 	}
-
+	
+	public String getCity() {
+		return city;
+	}
+	@Columns(columns = { @Column })
+	public String getFio() {
+		return fio;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public void setFio(String fio) {
+		this.fio = fio;
+	}
+	public static void main(String[] args) {
+		StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+		SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(new TestHibernate("НН", "Макс"));
+		session.getTransaction().commit();
+	}
 }
