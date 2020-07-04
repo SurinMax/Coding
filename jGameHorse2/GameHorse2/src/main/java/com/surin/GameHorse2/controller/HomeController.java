34e660919f9353ba0783f09e933f8c76e9f25e4c
SurinMax <60728730+SurinMax@users.noreply.github.com>
2020-07-04 18:19:23 +0300

    I just add two empty files game5x5 and game8x8

--- jGameHorse2/GameHorse2/src/main/java/com/surin/GameHorse2/controller/HomeController.java
+++ jGameHorse2/GameHorse2/src/main/java/com/surin/GameHorse2/controller/HomeController.java
@@ -2,8 +2,6 @@ package com.surin.GameHorse2.controller;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
